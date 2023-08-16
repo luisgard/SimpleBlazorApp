b31d52ef92c27f653db1b2b14d998cfa44a51a78
luisgard <lmgc_92@hotmail.com>
2023-08-16 06:26:25 -0600

    Add files via upload

---
+++ SimpleInterfaces/IProductosLogic.cs
@@ -0,0 +1,19 @@
+﻿using SimpleModelos;
+
+namespace SimpleInterfaces
+{
+    public interface IProductosLogic
+    {
+        Task<List<Producto>> GetProductosAsync();
+
+        Task<Producto> GetProductoAsync(Guid guid);
+
+        Task<(Producto produc, bool respueta)> AddProductoAsync(Producto producto);
+
+        Task<bool> DelProductoAsync(Producto producto);
+
+        Task<bool> EditProductoAsync(Producto producto);
+
+        Task<List<Categoria>> GetCategoriasAsync();
+    }
+}
